3aac5c85e5a4f9582f0ccb13280704b95bf62cc6
Jose Luis Marrero <marrerog.joseluis@gmail.com>
2016-04-06 22:32:50 -0300

    Primer comit

---
+++ EjercicioClase8/baseproject/EjercicioClase8/User.swift
@@ -0,0 +1,16 @@
+//
+//  Usuario.swift
+//  EjercicioClase8
+//
+//  Created by Jose Luis Marrero on 6/4/16.
+//
+//
+
+import Foundation
+
+class User {
+    
+    let name: String = ""
+    let surname: String = ""
+    
+}
